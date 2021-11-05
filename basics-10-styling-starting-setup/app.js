913634e715eddc5019b57dd16bdc7d27fa1ab036
sun <sun@sunui-MacBookPro-2.local>
2021-11-05 12:27:02 +0900

    box color toggle

---
+++ basics-10-styling-starting-setup/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false,
+        }
+    },
+    methods: {
+        boxSelected(box) {
+            if (box === 'A') {
+                console.log("A")
+                this.boxASelected = !this.boxASelected;
+            } else if (box === 'B') {
+                console.log("B")
+                this.boxBSelected = !this.boxBSelected;
+            } else if (box === 'C') {
+                console.log("C")
+                this.boxCSelected = !this.boxCSelected;
+            }
+        }
+    }
+});
+
+app.mount('#styling')
