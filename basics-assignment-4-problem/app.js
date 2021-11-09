bdbfdbe609b05fcd69c15ad17d66b68fcd5168a3
sun <sun@codebrick.co>
2021-11-09 14:01:44 +0900

    computed

--- basics-assignment-4-problem/app.js
+++ basics-assignment-4-problem/app.js
@@ -2,22 +2,18 @@ const app = Vue.createApp({
     data() {
         return {
             userInput: "",
-            user1boolean: false,
-            user2boolean: false,
             showParagraph: true,
         }
     },
-    watch: {
-        userInput() {
-            if (this.userInput === "user1") {
-                this.user1boolean = !this.user1boolean;
-            } else if (this.userInput === "user2") {
-                this.user2boolean = !this.user2boolean;
-            } else {
-                this.user1boolean = false;
-                this.user2boolean = false;
+    computed: {
+        computedClass() {
+            return {
+                user1: this.userInput === 'user1', 
+                user2: this.userInput === 'user2',
+                hidden: this.userInput === 'hidden',
             }
-        }},
+        }
+    },
     methods: {
         toggleParagraph() {
             this.showParagraph = !this.showParagraph;
