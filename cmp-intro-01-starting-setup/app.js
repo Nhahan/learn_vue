e3c61eac6ec9efd64849c8cda25d43b4f63c77e8
sun <sun@codebrick.co>
2021-11-12 17:11:06 +0900

    component practice

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
             friends: [
                 {
                     id: "manuel",
@@ -19,7 +18,36 @@ const app = Vue.createApp({
         };
     },
     methods: {
-        toggleDetails(id) {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+    },
+});
+
+app.component("friend-contact", {
+    template: `
+    <li v-for="friend in friends" :key="friend.id">
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> 01234 5678 991</li>
+            <li><strong>Email:</strong> manuel@localhost.com</li>
+        </ul>
+    </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friends: {
+                id: "manuel",
+                name: "Manuel Lorenz",
+                phone: "1234",
+                email: "manuel@localhost.com",
+            },
+        };
+    },
+    methods: {
+        toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
         },
     },
