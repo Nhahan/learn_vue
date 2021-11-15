6e43f2581e44283e2b2530219b91b0b389d17245
sun <sun@codebrick.co>
2021-11-15 12:27:05 +0900

    conponents: FriendContact.vue

---
+++ vue-complete/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    date() {
+        return {
+            friends: [
+                {
+                    id: "manuel",
+                    name: "Manuel Lorenz",
+                    phone: "1234",
+                    email: "manuel@localhost.com",
+                },
+                {
+                    id: "manuel2",
+                    name: "Manuel Lorenz2",
+                    phone: "5678",
+                    email: "manuel2@localhost.com",
+                },
+            ],
+        };
+    },
+};
+</script>
---
+++ vue-complete/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <h2>
+            {{ friend.name }}
+        </h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Eamil:</strong> {{ friend.eamil }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: "manuel2",
+                name: "Manuel Lorenz2",
+                phone: "5678",
+                email: "manuel2@localhost.com",
+            },
+        };
+    },
+    mehotds: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+    },
+};
+</script>
