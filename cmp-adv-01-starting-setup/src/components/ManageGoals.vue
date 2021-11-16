b47ba1d89c4742600b978442e4bc2b24bf669cae
sun <sun@codebrick.co>
2021-11-16 18:28:41 +0900

    ErrorAlert init

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
         <h2>ManageGoals</h2>
-        <input type="text" />
+        <input type="text" ref="goal" />
+        <button @click="setGoal">Set Goal</button>
     </div>
 </template>
+
+<script>
+export default defineComponent({
+    methods: {
+        setGoal() {
+            const enteredValue = this.$refs.goal.value;
+            if (enteredValue === "") {
+                alert("Input must not be empty");
+            }
+        },
+    },
+});
+</script>
