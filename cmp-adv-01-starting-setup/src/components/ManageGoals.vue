5fdab20848d72e7d60bc9a9526b83bb9df012cea
sun <sun@codebrick.co>
2021-11-16 18:45:03 +0900

    custom Error alert

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -6,6 +6,7 @@
         <error-alert v-if="inputIsInvalid">
             <h2>Input is invalid</h2>
             <p>Please enter at least a few Characters....</p>
+            <button @click="confirmError">Okay</button>
         </error-alert>
     </div>
 </template>
@@ -29,6 +30,9 @@ export default {
             if (enteredValue === "") {
                 this.inputIsInvalid = true;
             }
+        },
+        confirmError() {
+            this.inputIsInvalid = false;
         },
     },
 };
