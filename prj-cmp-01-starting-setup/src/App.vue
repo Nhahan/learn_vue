8209938c08994ff44249ff5386d104171d7c4970
sun <sun@codebrick.co>
2021-11-16 19:23:42 +0900

    init

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <LearningResource v-for="res in storedResources" :key="res.id" />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningItem.vue';
+
+export default {
+  components: { LearningResource },
+  data() {
+    return {
+      stroedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
