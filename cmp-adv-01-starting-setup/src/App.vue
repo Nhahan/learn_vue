c4c9a1d273f8ebefecda66d8df85f79751001294
sun <sun@codebrick.co>
2021-11-16 17:42:59 +0900

    scoped slots

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,35 +1,41 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-  </div>
+    <div>
+        <the-header></the-header>
+        <badge-list></badge-list>
+        <user-info
+            :full-name="activeUser.name"
+            :info-text="activeUser.description"
+            :role="activeUser.role"
+        ></user-info>
+        <course-goals>
+            <template #default="slotProps">
+                <h2>{{ slotProps.item }}</h2>
+                <h2>{{ slotProps.item }}</h2>
+            </template>
+        </course-goals>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
+    data() {
+        return {
+            activeUser: {
+                name: "Maximilian Schwarzmüller",
+                description: "Site owner and admin",
+                role: "admin",
+            },
+        };
+    },
 };
 </script>
 
 <style>
 html {
-  font-family: sans-serif;
+    font-family: sans-serif;
 }
 
 body {
-  margin: 0;
+    margin: 0;
 }
 </style>
--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -6,6 +6,7 @@ import BaseBadge from "./components/BaseBadge.vue";
 import BaseCard from "./components/BaseCard.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 const app = createApp(App);
 
@@ -14,5 +15,6 @@ app.component("base-badge", BaseBadge);
 app.component("base-card", BaseCard);
 app.component("badge-list", BadgeList);
 app.component("user-info", UserInfo);
+app.component("course-goals", CourseGoals);
 
 app.mount("#app");
