5ce625e17ff8f9d9ba9a9ac976f7396e572a333d
sun <sun@codebrick.co>
2021-11-17 17:46:57 +0900

    to prevent submit solo, add type=button property on the button

---
+++ forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li><button type="button">Poor</button></li>
+    <li><button type="button">Average</button></li>
+    <li><button type="button">Great</button></li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scroped>
+ul {
+  list-style: none;
+  margin: 0.5rem 0.5rem 0.5rem;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
