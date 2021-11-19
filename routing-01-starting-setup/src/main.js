48189d47cb783a47cc0df514505936de622aa8ff
sun <sun@codebrick.co>
2021-11-19 18:38:48 +0900

    passing data with route params

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,7 +16,11 @@ const router = createRouter({
     {
       path: '/users',
       component: UsersList,
-    }, // out-domain.com/users => TeamsList
+    }, // out-domain.com/users => UsersList
+    {
+      path: '/teams/:teamId',
+      component: TeamMembers,
+    }, // out-domain.com/users => UsersList
   ],
   linkActiveClass: 'active',
 });
