3674a84e4f00414c5bb7acec17282dd89b83aea7
sun <sun@codebrick.co>
2021-12-02 18:49:35 +0900

    init

--- main-prj-01-starting-setup/src/App.vue
+++ main-prj-01-starting-setup/src/App.vue
@@ -4,6 +4,17 @@
     </router-view>    
 </template>
 
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+    components: {
+        TheHeader
+    }
+}
+
+</script>
+
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
