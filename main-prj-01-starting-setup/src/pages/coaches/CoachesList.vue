70dd139e7095b4f6675b58e0bd2943e1e734c100
sun <sun@codebrick.co>
2021-12-07 18:08:47 +0900

    base

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -5,7 +5,10 @@
         </section>
             
         <section>
-            <div></div>
+            <div class="controls">
+                <button>Refresh</button>    
+                <router-link to="/register">Register as Coach</router-link>    
+            </div> 
             LIST OF COACHES
         </section>
     </div>
