d2390ec0a0396d0ceaa4478ef5c9be2cbf11fecf
sun <sun@codebrick.co>
2021-12-09 18:06:14 +0900

    s

--- main-prj-01-starting-setup/src/store/index.js
+++ main-prj-01-starting-setup/src/store/index.js
@@ -3,8 +3,9 @@ import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
 
 const store = createStore({
+    namespaced: true,
     modules: {
-        coachesModule
+        coachesModule,
     }
 });
 
