4ec5080bb5e86a998e971353741082152e86f7a2
sun <sun@codebrick.co>
2021-12-09 18:09:08 +0900

    bug fixed

--- main-prj-01-starting-setup/src/store/index.js
+++ main-prj-01-starting-setup/src/store/index.js
@@ -3,7 +3,6 @@ import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
 
 const store = createStore({
-    namespaced: true,
     modules: {
         coachesModule,
     }
