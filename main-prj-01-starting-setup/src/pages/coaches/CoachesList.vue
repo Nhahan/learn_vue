271941560cfd299bc62a0ceba519527970cb527d
sun <sun@codebrick.co>
2021-12-10 18:51:07 +0900

    hasCoaches()

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -9,9 +9,12 @@
                 <button>Refresh</button>    
                 <router-link to="/register">Register as Coach</router-link>    
             </div> 
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{ coach.firstName }}
-            </li>
+            <ul v-if="hasCoaches">
+                <li v-for="coach in filteredCoaches" :key="coach.id">
+                    {{ coach.firstName }}
+                </li>
+            </ul>
+            <h3 v-else>No coaches found.</h3>
         </section>
     </div>
 </template>
@@ -21,7 +24,10 @@ export default {
     computed: {
         filteredCoaches() {
             return this.$store.getters['coachesModule/coaches'];
-        }
+        },
+        hasCoaches() {
+            return this.$store.getters['coachesModule/hasCoaches'];
+        },
     }
 }
 </script>
