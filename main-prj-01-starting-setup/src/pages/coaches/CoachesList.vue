fb069f45bc1972036a293491bb81b71af53921e0
sun <sun@codebrick.co>
2021-12-14 18:44:20 +0900

    style

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -42,3 +42,15 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
